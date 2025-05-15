6454fa516e42c3a936e6873bb06d7edc10e19700
Felipe Komata <fbkomata@gmail.com>
2025-05-15 11:17:15 -0300

    criação dos nomes inseridos e iniciação do sortear

--- js/app.js
+++ js/app.js
@@ -1,22 +1,36 @@
-   let listaSelecionados = [];
+    let listaSorteados = [];
 
-function adicionar(){
-    let nomeAmigo = document.querySelector('.form__input').value;
-    listaSelecionados.push(nomeAmigo)
+   function adicionar(){
+    let nomeAmigo = document.getElementById('nome-amigo');
     let listaAmigos = document.getElementById('lista-amigos');
-    listaAmigos.innerHTML = listaSelecionados
-    
-    
-    console.log(nomeAmigo)
-    console.log(listaAmigos)
+    listaSorteados.push(nomeAmigo.value);
 
-
+    if(listaAmigos.textContent == ''){
+      listaAmigos.textContent = nomeAmigo.value;
+    }else{
+      listaAmigos.textContent = listaAmigos.textContent + ', ' + nomeAmigo.value;
+    }
+     nomeAmigo.value = '';
+    
 }
 
 function sortear(){
+  let sorteados = embaralhar(listaSorteados);
 
+  for(i=0 ; i=listaSorteados; i++){
+  }
+  
+  let nomes = sorteados.textContent.split(',' , 2);
+
+   
 }
 
 function reiniciar(){
+}
 
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
